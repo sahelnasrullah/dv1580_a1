8f8f18b58075a091f83942bbd50fbf340063e35b
sahelnasrullah <sahelnasrullah@hotmail.com>
2024-09-25 17:01:43 +0200

    mm

--- linked_list.h
+++ linked_list.h
@@ -3,21 +3,20 @@
 
 #include <stdint.h>
 
-
 typedef struct Node {
     uint16_t data;
     struct Node* next;
 } Node;
 
-void list_init(Node** head);
-void list_insert(Node** head, int data);
-void list_insert_after(Node* prev_node, int data);
-void list_insert_before(Node** head, Node* next_node, int data);
-void list_delete(Node** head, int data);
+void list_init(Node** head, size_t node_size);
+void list_insert(Node** head, int data, size_t node_size);
+void list_insert_after(Node* prev_node, int data, size_t node_size);
+void list_insert_before(Node** head, Node* next_node, int data, size_t node_size);
+void list_delete(Node** head, int data, size_t node_size);
 Node* list_search(Node** head, int data);
 void list_display(Node** head);
 void list_display_range(Node** head, Node* start_node, Node* end_node);
 int list_count_nodes(Node** head);
-void list_cleanup(Node** head);
+void list_cleanup(Node** head, size_t node_size);
 
 #endif
