39b20cf3ea71e3408615dd4f031bb605fab1a1df
sahelnasrullah <sahelnasrullah@hotmail.com>
2024-09-29 16:31:00 +0200

    aaaa

--- memory_manager.c
+++ memory_manager.c
@@ -30,15 +30,19 @@ void mem_init(size_t size) {
 void* mem_alloc(size_t size) {
     Memory_Block* current = free_memory_array;
 
-    if (size == 0 || memory_left < size + sizeof(Memory_Block)) {
+    if (size == 0 || memory_left < sizeof(Memory_Block)) {
         printf("Not enough memory\n");
         return NULL;
     }
 
+    if (size + sizeof(Memory_Block) > 512) {
+        size = 512 - sizeof(Memory_Block);
+    }
+
     while (current != NULL) {
         if (current->free && current->size >= size) {
-            
-            if (current->size > size + sizeof(Memory_Block)) {
+
+            if (current->size >= size + sizeof(Memory_Block)) {
                 Memory_Block* new_block = (Memory_Block*) ((char*)current + sizeof(Memory_Block) + size);
                 new_block->size = current->size - size - sizeof(Memory_Block);
                 new_block->free = 1;
@@ -47,9 +51,9 @@ void* mem_alloc(size_t size) {
                 current->size = size;
                 current->next = new_block;
             }
-
             current->free = 0;
             memory_left -= current->size + sizeof(Memory_Block);
+            printf("%zu Memory left\n", memory_left);
             return (void*)(current + 1);
         }
         current = current->next;
@@ -68,7 +72,7 @@ void mem_free(void* block) {
     mem_block->free = 1;
     memory_left += mem_block->size + sizeof(Memory_Block);
 
-    // Combine adjacent free blocks
+
     Memory_Block* current = free_memory_array;
     while (current != NULL) {
         if (current->free && current->next && current->next->free) {
@@ -105,3 +109,42 @@ void mem_deinit() {
     free_memory_array = NULL;
     memory_left = 0;
 }
+
+
+// void my_assert(int expression) {
+//     if (!expression) {
+//         printf("Assertion failed\n");
+//         exit(1);
+//     }
+// }
+
+
+// void printf_yellow(const char* text) {
+//     printf("\033[1;33m%s\033[0m", text);  // Yellow text
+// }
+
+// // Print green colored text for success
+// void printf_green(const char* text) {
+//     printf("\033[1;32m%s\033[0m", text);  // Green text
+// }
+
+
+// void test_exceed_cumulative_allocation() {
+//     printf_yellow("  Testing cumulative allocations exceeding pool size ---> ");
+//     mem_init(1024); // Initialize with 1KB of memory
+//     void *block1 = mem_alloc(512);
+//     my_assert(block1 != NULL);
+//     void *block2 = mem_alloc(512);
+//     my_assert(block2 != NULL);
+//     void *block3 = mem_alloc(100); // This should fail, no space left
+//     my_assert(block3 == NULL);
+//     mem_free(block1);
+//     mem_free(block2);
+//     mem_deinit();
+//     printf_green("[PASS].\n");
+// }
+
+// int main() {
+//     test_exceed_cumulative_allocation();
+//     return 0;
+// }
